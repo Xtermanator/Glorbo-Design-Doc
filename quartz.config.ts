6c000f3d1e03755ae7cb98014ad62b403f34071a
James Moran <jameslearn1@gmail.com>
2026-01-30 23:39:45 -0500

    Update quartz.config.ts
    Changed title of site

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Glorbo Design Document",
+    pageTitle: "Glorbo",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
